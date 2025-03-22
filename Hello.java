eacf32faa57b7335392302e1d8b7beeaeb1d74fe
prathmeshubale <145438245+prathmeshubale@users.noreply.github.com>
2025-03-22 12:24:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@ System.out.println("Hello, World!");
   System.out.println("Cheking Continues Integration!");
   System.out.println("Wow Its Done !");
   System.out.println("That's For The Day!");
+  System.out.println("Today I'm working on PetClinic project");
+  
   
 }
 }
