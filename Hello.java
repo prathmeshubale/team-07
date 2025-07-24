4b6ac0225918fe2b503f8aca483b61eb88b67f29
prathmeshubale <145438245+prathmeshubale@users.noreply.github.com>
2025-07-24 18:48:40 +0530

    Update5 Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
 System.out.println("Hello, World!");
   System.out.println("Cheking Continues Integration!");
   System.out.println("Wow Its Done !");
-  System.out.println("That's For The Day!")
+  System.out.println("That's For The Day!");
   
   
   
